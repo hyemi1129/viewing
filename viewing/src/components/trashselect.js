318f24d9c9bf0b03f51c41c7210ea20e885ff446
hyemi1129 <kimhyemi1243@gmail.com>
2024-07-11 19:56:45 +0900

    0711

--- viewing/src/components/trashselect.js
+++ viewing/src/components/trashselect.js
@@ -3,11 +3,9 @@ import React from 'react';
 const TrashSelect = () =>{
 
     return(
-      <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', height: '100vh' }}>
        <div style={{ marginRight: '150px' }}>
-        <h1 style={{ paddingLeft: '150px', marginTop: '20px', fontSize: '45px' }}>분리수거 할 쓰레기의 종류를<br />선택해주세요</h1>
+        <h1 style={{ paddingLeft: '150px', marginTop: '70px', fontSize: '55px', textAlign: 'left' }}>분리수거 할 <span className = "highlight">쓰레기의 종류</span>를<br /> 선택해주세요</h1>
       </div>
-    </div>
     )
 }
 
